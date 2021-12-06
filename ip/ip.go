0112268fafed864308be2c3d0c5d0ffa0db5a375
jimyag <jimyag@126.com>
2021-12-06 15:36:33 +0800

    feat: 重写获取ip方法
    BREAKING CHANGE: 此前获取ip方法不适用，并引入新方法

--- ip/ip.go
+++ ip/ip.go
@@ -1,11 +1,47 @@
 package ip
 
-import "net"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
 
-type myIp struct {
-	Ip
+func GetExternalIp() (string, error) {
+	//resp, err := http.Get("https://myexternalip.com/raw")
+	resp, err := http.Get("https://www.taobao.com/help/getip.php")
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+		}
+	}(resp.Body)
+	c, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	content := string(c)
+	content = content[16 : len(content)-3]
+	return content, nil
 }
 
-func (this *myIp) GetExternalIp() (net.IP, error) {
-	return this.Ip.GetExternalIp()
+func GetIpMsg(ip string) (string, error) {
+	//https://ip.taobao.com/outGetIpInfo?ip=&accessKey=alibaba-inc
+	resp, err := http.Get(fmt.Sprintf("https://ip.taobao.com/outGetIpInfo?ip=%s&accessKey=alibaba-inc", ip))
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+		}
+	}(resp.Body)
+	c, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(c), nil
+
 }
